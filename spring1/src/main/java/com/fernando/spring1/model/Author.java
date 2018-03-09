5c4b0b09824fdb52e655d1d10009945b074b1446
fviel <fviel@ecelepar20574>
2018-03-09 15:15:54 -0300

    Revisão de anotações de Author

--- spring1/src/main/java/com/fernando/spring1/model/Author.java
+++ spring1/src/main/java/com/fernando/spring1/model/Author.java
@@ -6,9 +6,9 @@
 package com.fernando.spring1.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -26,13 +27,21 @@ import javax.persistence.Table;
 public class Author implements Serializable{
 
     private static final long serialVersionUID = 3783093145423408126L;
-        
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+	 @Column(name = "id_author")
     private Long idAuthor;
+
+	 @NotNull
+	 @Column(name = "first_name")
     private String firstName;
+
+	 @NotNull
+	 @Column(name = "last_name")
     private String lastName;
 
+	 @NotNull
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
 
