79fdaf8fdfb2be7b9bd9980dd26503b3800b0e0a
fviel <fviel@ecelepar20574>
2018-03-09 15:14:22 -0300

    Revisão de anotações de Book

--- spring1/src/main/java/com/fernando/spring1/model/Book.java
+++ spring1/src/main/java/com/fernando/spring1/model/Book.java
@@ -8,6 +8,7 @@ package com.fernando.spring1.model;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,23 +19,31 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  *
  * @author Fernando
  */
-@Entity 
+@Entity
 @Table (name="tb_book"/*, schema="test_project"*/)
 public class Book implements Serializable{
 
     private static final long serialVersionUID = -1313921026471428376L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long idBook;
+	 @Column(name = "id_book")
+	 private Long idBook;
+
+	 @NotNull
+	 @Column(name = "title")
     private String title;
+
+	 @NotNull
+	 @Column(name = "isbn")
     private String isbn;
-    
+
     @OneToOne
     @JoinColumn(name="idPublisher")
     private Publisher publisher;
