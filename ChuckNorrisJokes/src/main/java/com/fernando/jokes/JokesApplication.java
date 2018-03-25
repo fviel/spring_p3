dd202515488f6a9d358c1076d661559a1e26361c
Fernando Viel <nandoviel@hotmail.com>
2018-03-25 15:12:23 -0300

    Removi a isntanciação do context para pegar o bean de propriedades no arquivo main, pois via autowired na contrutora já o obtive.

--- ChuckNorrisJokes/src/main/java/com/fernando/jokes/JokesApplication.java
+++ ChuckNorrisJokes/src/main/java/com/fernando/jokes/JokesApplication.java
@@ -9,12 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class JokesApplication {
 
     public static void main(String[] args) {
-
-        //joga o resultado da execução para o applicationContext do spring para poder trabalhar ele
-        ApplicationContext ctx = SpringApplication.run(JokesApplication.class, args);
-        
-         Propriedades p = (Propriedades) ctx.getBean(Propriedades.class);
-
         SpringApplication.run(JokesApplication.class, args);
     }
 }
